db0477799dd34796c92947b92dd8d3f5db427948
Wistak <piotr.slomczynski@erpatech.com.pl>
2024-10-28 15:01:42 +0100

    Add public

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const users = [];
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 const server = app.listen(process.env.PORT || 8000, () => {
